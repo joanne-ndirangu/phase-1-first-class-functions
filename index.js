dcc5a217d600200520f3598bcff1c7b99c16f230
Joanne Ndirangu <joannendirangu15@gmail.com>
2023-06-15 22:29:51 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,19 @@
+function receivesAFunction(callback) {
+    callback();
+}
+
+function returnsANamedFunction(fn) {
+    return fn();
+}
+
+function returnsANamedFunction() {
+  return function namedFunction() {
+    console.log("This is a named function.");
+  };
+}
+
+function returnsAnAnonymousFunction() {
+  return function() {
+    console.log("This is an anonymous function.");
+  };
+}
